c92785b55d7ab68a3cc924d58d41b025ca758aba
Matt Varblow <mvarblow@users.noreply.github.com>
2025-04-15 11:47:38 -0400

    Initial commit

---
+++ InMemoryRAG/Movie.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.VectorData;
+using System;
+
+namespace InMemoryRAG;
+
+public class Movie
+{
+    [VectorStoreRecordKey]
+    public int Key { get; set; }
+
+    [VectorStoreRecordData]
+    public required string Title { get; set; }
+
+    [VectorStoreRecordData]
+    public required string Description { get; set; }
+
+    [VectorStoreRecordVector(384, DistanceFunction.CosineSimilarity)]
+    public ReadOnlyMemory<float> Vector { get; set; }
+}
